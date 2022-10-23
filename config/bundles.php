e9ffa32e6afd6ed7aa1c40c1e8677d4dd04cd53e
hjacquir <jacquirhatim@gmail.com>
2022-10-23 09:16:27 +0200

    [Application] install symfony security component

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
